9a7f22c8cbbb9cf43bf5941485526ddb11265b17
FelippeChemello <felippechemello@gmail.com>
2019-05-10 04:04:01 +0000

    testes

--- index.js
+++ index.js
@@ -3,16 +3,18 @@ const robots = {
   text: require('./robots/text.js'),
   state: require('./robots/state.js'),
   image: require('./robots/image.js'),
+  voice: require('./robots/voice'),
   video: require('./robots/video.js'),
   youtube: require('./robots/youtube.js')
 }
 
 async function start() {
-  robots.input()
-  await robots.text()
-  await robots.image()
+  //robots.input()
+  //await robots.text()
+  //await robots.image()
+  //await robots.voice()
   await robots.video()
-  await robots.youtube()
+  //await robots.youtube()
 }
 
 start()
