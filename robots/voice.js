788e72428a9a505049bd7407d12c1c2323cd0fed
felippe <felippe.chemello@kinghost.com.br>
2019-05-10 14:58:30 -0300

    criado verificação se todos os audios foram criados; alterado output; escolhe qual voz tem preferencia de uso

--- robots/voice.js
+++ robots/voice.js
@@ -2,6 +2,7 @@ const googleTTS = require('google-tts-api');
 const textToSpeech = require('@google-cloud/text-to-speech');
 const fs = require('fs');
 const download = require('download-file')
+const audioconcat = require('audioconcat')
 
 const state = require('./state.js');
 
@@ -11,18 +12,57 @@ async function robot() {
     console.log('> [voice-robot] Starting...');
     const content = state.load();
 
+    let voices = [];
+    let sentenceLenght;
+
+    if(content.voice === "Paga" || content.voice === "Paid"){
+        sentenceLenght = 0;
+    }else{
+        sentenceLenght = 200;
+    }
+
     for (let sentenceIndex = 0; sentenceIndex < content.sentences.length; sentenceIndex++) {
 
         console.log(`> [voice-robot] Sentence ${sentenceIndex}`);
         let text = content.sentences[sentenceIndex].text;
 
-        console.log(text);
-
-        if (text.length < 200) {
+        if (text.length < sentenceLenght) {
             await freeGoogleTTS(text, sentenceIndex);
         } else {
             await paidGoogleTTS(text, sentenceIndex);
         }
+
+    }
+
+    let i = 0;
+    do{
+        if(fs.existsSync(`./content/output[${i}].mp3`)){
+            voices.push(`./content/output[${i}].mp3`);
+            console.log(`> [voice-robot] output[${i}].mp3 confirmed`);
+            i++;
+        }else{
+            console.error(`> [voice-robot] output[${i}].mp3 doesn't exist`);
+            await sleep(1000);
+        }
+    }while (i < content.sentences.length);
+
+    audioconcat(voices)
+        .concat('./content/output[final].mp3')
+        .on('start', function (command) {
+            console.log('> [voice-robot] Voices concat started, command: ', command)
+        })
+        .on('error', function (err, stdout, stderr) {
+            console.error('Error:', err)
+            console.error('ffmpeg stderr:', stderr)
+        })
+        .on('end', function (output) {
+            console.error('> [voice-robot] Audio created in: /content/output[final].mp3', output)
+        })
+
+
+
+    function sleep(ms) {
+        return new Promise(resolve => setTimeout(resolve, ms));
     }
 
     async function freeGoogleTTS(text, sentenceIndex) {
@@ -30,7 +70,7 @@ async function robot() {
 
         let options = {
             directory: ".",
-            filename: "output[" + sentenceIndex + "].mp3"
+            filename: "./content/output[" + sentenceIndex + "].mp3"
         };
 
         await googleTTS(text, 'pt', 1).then(async function (url) {
@@ -66,7 +106,7 @@ async function robot() {
                 console.log(`> [voice-robot] Sentence ${sentenceIndex} synthesize complete`);
             }
 
-            await fs.writeFile(`output[${sentenceIndex}].mp3`, response.audioContent, 'binary', err => {
+            await fs.writeFile(`./content/output[${sentenceIndex}].mp3`, response.audioContent, 'binary', err => {
                 if (err) {
                     console.log(`> [voice-robot] Sentence ${sentenceIndex} error - ${err}`)
                 } else{
