73d1f215f8bc0932e401a2c45b2fa2cc34217e82
felippe <felippe.chemello@kinghost.com.br>
2019-05-10 14:48:44 -0300

    Adicionado robô start

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const robots = {
+  start: require('./robots/start.js'),
   input: require('./robots/input.js'),
   text: require('./robots/text.js'),
   state: require('./robots/state.js'),
@@ -8,13 +9,17 @@ const robots = {
   youtube: require('./robots/youtube.js')
 }
 
+
+
 async function start() {
-  //robots.input()
-  //await robots.text()
-  //await robots.image()
-  //await robots.voice()
+
+  await robots.start()
+  robots.input()
+  await robots.text()
+  await robots.image()
+  await robots.voice()
   await robots.video()
-  //await robots.youtube()
+  await robots.youtube()
 }
 
 start()
