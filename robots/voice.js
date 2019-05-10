a7d24883fc723086260195ad88cf966a969f2e50
FelippeChemello <felippechemello@gmail.com>
2019-05-10 04:04:45 +0000

    Funçoes

--- robots/voice.js
+++ robots/voice.js
@@ -19,50 +19,61 @@ async function robot() {
         console.log(text);
 
         if (text.length < 200) {
-
-            console.log('> [voice-robot] Free request');
-
-            let options = {
-                directory: ".",
-                filename: "output[" + sentenceIndex + "].mp3"
-            };
-
-            googleTTS(text, 'pt', 1).then(function (url) {
-                download(url, options, function(err){
-                    if (err) {
-                        console.log(`> [voice-robot] Sentence ${sentenceIndex} transcript error `);
-                    }
-                });
-                console.log(`> [voice-robot] output[${sentenceIndex}].mp3 successful download `);
-            });
+            await freeGoogleTTS(text, sentenceIndex);
         } else {
-
-            console.log('> [voice-robot] Paid request');
-
-            const client = new textToSpeech.TextToSpeechClient();
-
-            const request = {
-                input: {text: text},
-                voice: {
-                    languageCode: 'pt-BR',
-                    name: "pt-BR-Wavenet-A"
-                },
-                audioConfig: {audioEncoding: 'MP3'}
-            };
-
-            client.synthesizeSpeech(request, (err, response) => {
-                if (err) {
-                    console.log(`> [voice-robot] Sentence ${sentenceIndex} transcript error `)
-                }
-
-                fs.writeFile(`output[${sentenceIndex}].mp3`, response.audioContent, 'binary', err => {
-                    if (err) {
-                        console.log(`> [voice-robot] Sentence ${sentenceIndex} transcript error `)
-                    }
-                    console.log(`> [voice-robot] output[${sentenceIndex}].mp3 successful download `);
-                });
-            });
+            await paidGoogleTTS(text, sentenceIndex);
         }
+    }
+
+    async function freeGoogleTTS(text, sentenceIndex) {
+        console.log('> [voice-robot] Free request');
+
+        let options = {
+            directory: ".",
+            filename: "output[" + sentenceIndex + "].mp3"
+        };
+
+        await googleTTS(text, 'pt', 1).then(async function (url) {
+            await download(url, options, function(err){
+                if (err) {
+                    console.log(`> [voice-robot] Sentence ${sentenceIndex} download error `);
+                }else{
+                    console.log(`> [voice-robot] output[${sentenceIndex}].mp3 successful download `);
+                }
+            });
+        });
+    }
+
+    async function paidGoogleTTS(text, sentenceIndex) {
+
+        console.log('> [voice-robot] Paid request');
+
+        const client = new textToSpeech.TextToSpeechClient();
+
+        const request = {
+            input: {text: text},
+            voice: {
+                languageCode: 'pt-BR',
+                name: "pt-BR-Wavenet-A"
+            },
+            audioConfig: {audioEncoding: 'MP3'}
+        };
+
+        await client.synthesizeSpeech(request, async (err, response) => {
+            if (err) {
+                console.log(`> [voice-robot] Sentence ${sentenceIndex} synthesize error - ${err}`);
+            }else{
+                console.log(`> [voice-robot] Sentence ${sentenceIndex} synthesize complete`);
+            }
+
+            await fs.writeFile(`output[${sentenceIndex}].mp3`, response.audioContent, 'binary', err => {
+                if (err) {
+                    console.log(`> [voice-robot] Sentence ${sentenceIndex} error - ${err}`)
+                } else{
+                    console.log(`> [voice-robot] output[${sentenceIndex}].mp3 successful download `);
+                }
+            });
+        });
     }
 
 }
