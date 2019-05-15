651fb4cb6380538d1dfdfdf2263efd0633455ca2
felippe <felippe.chemello@kinghost.com.br>
2019-05-15 15:28:20 -0300

    comentado trends.js

--- index.js
+++ index.js
@@ -1,25 +1,26 @@
 const robots = {
-  start: require('./robots/start.js'),
-  input: require('./robots/input.js'),
-  text: require('./robots/text.js'),
-  state: require('./robots/state.js'),
-  image: require('./robots/image.js'),
-  voice: require('./robots/voice'),
-  video: require('./robots/video.js'),
-  youtube: require('./robots/youtube.js')
-}
-
+    start: require('./robots/start.js'),
+    input: require('./robots/input.js'),
+    trends: require('./robots/trends'),
+    text: require('./robots/text.js'),
+    state: require('./robots/state.js'),
+    image: require('./robots/image.js'),
+    voice: require('./robots/voice'),
+    video: require('./robots/video.js'),
+    youtube: require('./robots/youtube.js')
+};
 
 
 async function start() {
 
-  await robots.start()
-  robots.input()
-  await robots.text()
-  await robots.image()
-  await robots.voice()
-  await robots.video()
-  await robots.youtube()
+    await robots.start();
+    robots.input();
+    //await robots.trends();
+    await robots.text();
+    await robots.image();
+    await robots.voice();
+    await robots.video();
+    await robots.youtube()
 }
 
-start()
+start();
