c185013aeb06c3d6d9b594529c170268f4044a4b
Felippe Chemello <felippe.chemello@kinghost.com.br>
2019-05-23 15:42:43 -0300

    alterado para testar database

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const robots = {
     start: require('./robots/start.js'),
     input: require('./robots/input.js'),
     trends: require('./robots/trends'),
+    database: require('./robots/database'),
     text: require('./robots/text.js'),
     state: require('./robots/state.js'),
     image: require('./robots/image.js'),
@@ -13,14 +14,16 @@ const robots = {
 
 async function start() {
 
-    await robots.start();
-    robots.input();
-    await robots.trends();
-    await robots.text();
-    await robots.image();
-    await robots.voice();
-    await robots.video();
-    await robots.youtube()
+    robots.database.saveBaseData();
+
+    // await robots.start();
+    // await robots.input();
+    // await robots.trends();
+    // await robots.text();
+    // await robots.image();
+    // await robots.voice();
+    // await robots.video();
+    // await robots.youtube()
 }
 
 start();
